2f548e0863f5c878df487ec3550e49b01749c603
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2024-04-15 22:09:13 +0530

    next-auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { signIn, useSession } from 'next-auth/react'
 import React from 'react'
 
 const LoginPage = () => {
+  const {data,status}= useSession()
+  console.log(data,status)
+
+  const googleLogin =()=>{
+    signIn("google")
+  }
+
   return (
     <div className='rounded-sm grid place-items-center gap-10'>
         <h1 className='text-3xl mb-10 mt-5'>Login</h1>
-        <button className='bg-red-500 w-36 text-center p-3 rounded-md text-2xl font-medium' >Google</button>
+        <button className='bg-red-500 w-36 text-center p-3 rounded-md text-2xl font-medium' onClick={googleLogin} >Google</button>
         <button className='bg-black w-36 text-center p-3 rounded-md text-white text-2xl font-medium'>Github</button>
         <button className='bg-blue-500 w-36 text-center p-3 rounded-md text-2xl font-medium'>Facebook</button>
     </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,29 @@
 'use client'
+import { signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
   
   const router = useRouter()
+
+  const {data,status}= useSession()
+  console.log(data,status)
+  if(status=="authenticated"){
+      router.push("/")
+  }
+  if(status=="unauthenticated"){
+    router.push("/login")
+  }
   return (
     <div className='bg-slate-300 flex-col justify-between'>
     <span onClick={()=>router.push('/')} className='text-3xl text-center font-semibold m-3 hover:cursor-pointer'>Talkbills</span>
 
-    <button className='bg-blue-300 rounded-md p-2 m-3' onClick={()=>router.push('/login')}>Login</button>
+    {status=="authenticated" && 
+    <button className='bg-blue-300 rounded-md p-2 m-3' onClick={()=>signOut()}>logout</button>
+  }
+    {status == "unauthenticated" &&
+    <button className='bg-blue-300 rounded-md p-2 m-3' onClick={()=>router.push('/login')}>Login</button>}
     </div>
   )
 }
